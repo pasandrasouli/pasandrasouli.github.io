0893cac605cd5692fef330673f73c0f3f99e7416
Soltani Reza <soltanireza65@gmail.com>
2026-06-30 01:54:31 +0330

    chore

--- src/data/projects/oms-xanis.ts
+++ src/data/projects/oms-xanis.ts
@@ -2,18 +2,172 @@ import { Project } from '../../types'
 
 export const omsXanis: Project = {
   id: 5,
-  title: "OMS Xanis",
-  category: "",
-  coverImage: "./public/assets/images/Cover/transaction.png",
-  fullDescription: "",
+  title: 'OMS Xanis',
+  category: '',
+  coverImage: '',
+  fullDescription: '',
   results: [],
   caseStudy: [
     {
       type: 'section',
       title: 'Product Overview',
-      imageSrc: '',
+      imageSrc: 'https://placehold.co/600x200/EEE/31343C',
       description:
         'Xino Trader is an advanced trading platform with a user-friendly interface. It enables users to trade in various markets with ease and efficiency. Users can follow symbols, view their details, and execute buy and sell orders. The platform offers unique features such as placing multiple orders simultaneously and breaking orders for better trade management. The primary objective of Xino Trader is to provide users with a fast, efficient, and effective trading experience.',
+    },
+    {
+      type: 'section',
+      title: 'Problem Statement',
+      imageSrc: 'https://placehold.co/600x200/EEE/31343C',
+      description:
+        'One of the challenges that users face on the trading platforms is the time-consuming process of placing multiple orders simultaneously. When users attempt to submit several orders at once, it often takes a significant amount of time, elongating the order placement process. Additionally, users may encounter limitations on the allowed trading volume, preventing them from executing trades with volumes larger than the specified limits.',
+    },
+    {
+      type: 'section',
+      title: 'Goals',
+      imageSrc: 'https://placehold.co/600x200/EEE/31343C',
+      description:
+        'Enhancing the trading experience on the Xino platform by optimizing the process of placing multiple orders simultaneously, reducing time, and allowing trading with volumes beyond predefined limits.',
+    },
+    {
+      type: 'section',
+      title: 'Design Process',
+      imageSrc: 'https://placehold.co/600x200/EEE/31343C',
+      description:
+        "The design process for the Xino trading platform involved extensive user research to understand trader needs and preferences. Clear design goals were defined, and wireframes and prototypes were created and iterated upon based on user feedback. The final interface designs were visually appealing and aligned with the platform's brand identity. Usability testing sessions were conducted, and user feedback was integrated to make necessary adjustments. The updated trading feature was carefully implemented and continuously monitored to ensure optimal user experience and engagement.",
+    },
+    {
+      type: 'section',
+      title: 'Design Timeline',
+      imageSrc: 'https://placehold.co/600x200/EEE/31343C',
+      description: "",
+    },
+    {
+      type: 'section',
+      title: 'Target Audience',
+      imageSrc: '',
+      description: "The goal of the xino trading platform is to attract professional and experienced traders who are seeking a user-friendly trading experience. The platform is suitable for both experienced traders and beginners, offering advanced features such as the ability to place multiple orders simultaneously. The primary objective of Xino Trader is to improve traders' experience by providing fast and efficient trade execution.",
+    },
+    {
+      type: 'section',
+      title: 'Competitive Analysis',
+      imageSrc: 'https://placehold.co/600x200/EEE/31343C',
+      description: "",
+    },
+   
+    {
+      type: 'section',
+      title: 'Quantitative Research',
+      imageSrc: '',
+      description: "For the Quantitative Research section, we will focus on investigating the usage and user experience of the trading feature on the Xino platform. The primary objectives of this research are to determine the proportion of users utilizing the trading feature, assess their level of satisfaction, and identify any potential issues or challenges they encounter.",
+    },
+    {
+      type: 'withBullet',
+      title: 'Research Questions',
+      titleGroups:[],
+      items:[
+        "What percentage of the current platform users actively engage with the trading feature?",
+        "How satisfied are the users with their experience using the trading feature?",
+        "Is the process of order placement and execution efficient and prompt?",
+        "Have users been able to execute trades with volumes beyond the specified limits?",
+        ""
+      ]
+    },
+    {
+      type: 'section',
+      title: 'Observations',
+      imageSrc: 'https://placehold.co/600x200/EEE/31343C',
+      description: "",
+    },
+    {
+      type: 'section',
+      title: 'Empathy Map',
+      imageSrc: 'https://placehold.co/600x200/EEE/31343C',
+      description: "",
+    },
+    {
+      type: 'section',
+      title: 'Pain Points',
+      imageSrc: 'https://placehold.co/600x200/EEE/31343C',
+      description: "",
+    },
+    {
+      type: 'section',
+      title: 'User Persona',
+      imageSrc: 'https://placehold.co/600x200/EEE/31343C',
+      description: "",
+    },
+    {
+      type: 'section',
+      title: 'User Journey Map',
+      imageSrc: '',
+      description: "",
+    },
+    {
+      type: 'section',
+      title: 'Persona: Tara',
+      imageSrc: 'https://placehold.co/600x600/EEE/31343C',
+      description: "Goal: Quick and efficient trading with the ability to place multiple orders simultaneously to leverage diverse opportunities and enhance user experience.",
+    },
+    {
+      type: 'section',
+      title: 'Information Architecture',
+      imageSrc: 'https://placehold.co/600x600/EEE/31343C',
+      description: "",
+    },
+    {
+      type: 'section',
+      title: 'Low-fidelity Wireframes',
+      imageSrc: 'https://placehold.co/600x600/EEE/31343C',
+      description: "",
+    },
+    {
+      type: 'section',
+      title: 'High Fidelity Screen',
+      imageSrc: 'https://placehold.co/600x600/EEE/31343C',
+      description: "",
+    },
+    {
+      type: 'section',
+      title: 'High Fidelity Screen',
+      imageSrc: 'https://placehold.co/600x600/EEE/31343C',
+      description: "",
+    },
+    {
+      type: 'section',
+      title: 'High Fidelity Screen',
+      imageSrc: 'https://placehold.co/600x600/EEE/31343C',
+      description: "",
+    },
+    {
+      type: 'section',
+      title: 'Other Screens',
+      imageSrc: 'https://placehold.co/600x600/EEE/31343C',
+      description: "The platform has been designed with both light and dark themes to ensure a seamless experience for users across all pages and windows. ",
+    },
+    {
+      type: 'section',
+      title: 'Style Guide',
+      imageSrc: 'https://placehold.co/600x600/EEE/31343C',
+      description: "",
+    },
+    {
+      type: 'section',
+      title: 'Color Palette',
+      imageSrc: 'https://placehold.co/600x600/EEE/31343C',
+      description: "",
+    },
+    {
+      type: 'section',
+      title: 'Typography',
+      imageSrc: 'https://placehold.co/600x600/EEE/31343C',
+      description: "",
+    },
+    {
+      type: 'section',
+      title: 'Iconography',
+      imageSrc: 'https://placehold.co/600x600/EEE/31343C',
+      description: "",
     },
   ],
 }
