07785168f5e4d0d6eed0d7c9ce2ce32fe6a5776f
Soltani Reza <soltanireza65@gmail.com>
2026-06-30 01:18:07 +0330

    chore

--- src/components/ProjectDetailPage.vue
+++ src/components/ProjectDetailPage.vue
@@ -47,25 +47,6 @@ function openProject(id: number) {
           <div v-if="project.caseStudy?.length" class="prose prose-invert max-w-none">
             <CaseStudyBlock v-for="(block, index) in project.caseStudy" :key="index" :block="block" />
           </div>
-
-          <div v-else>
-            <div class="grid md:grid-cols-2 gap-8 mt-10">
-              <div class="glass rounded-2xl p-6">
-                <h3 class="text-xl font-medium mb-2">Challenge</h3>
-                <p class="text-white/50">{{ project.challenge }}</p>
-              </div>
-              <div class="glass rounded-2xl p-6">
-                <h3 class="text-xl font-medium mb-2">Solution</h3>
-                <p class="text-white/50">{{ project.solution }}</p>
-              </div>
-            </div>
-            <div class="mt-10">
-              <h3 class="text-2xl font-medium mb-4">Key results</h3>
-              <ul class="list-disc list-inside text-white/60 space-y-2">
-                <li v-for="result in project.results" :key="result">{{ result }}</li>
-              </ul>
-            </div>
-          </div>
         </div>
       </div>
 
